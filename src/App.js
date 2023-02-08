da9a7b6a8e9deee55eee1f42c839d13da1dc9d68
Rubical <zhpiv96@mail.ru>
2023-02-08 09:12:28 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import SearchUser from "./components/SearchUser/SearchUser";
+// import SearchUser from "./components/SearchUser/SearchUser";
 
-function App() {
-  return <SearchUser />;
-}
+// function App() {
+//   return <SearchUser />;
+// }
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp/SignUp";
 import SignIn from "./pages/SignIn/SignIn";
 
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<SignUp />} />
-//         <Route path="signin" element={<SignIn />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
